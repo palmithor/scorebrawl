72b6763f58bcb119d6fe370f37a3fa894321834b
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2023-08-23 09:33:58 +0000

    League logo uploadthing router

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -1,3 +1,4 @@
+import { getAuth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
 
 const f = createUploadthing();
@@ -5,17 +6,17 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "1MB" } })
+  leagueLogo: f({ image: { maxFileSize: "1MB", maxFileCount: 1 } })
     // Set permissions and file types for this FileRoute
-    .middleware(({ req, res }) => {
+    .middleware(({ req }) => {
       // This code runs on your server before upload
-      // const user = await auth(req, res);
 
+      const { userId } = getAuth(req);
       // If you throw, the user will not be able to upload
-      //if (!user) throw new Error("Unauthorized");
+      if (!userId) throw new Error("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: "test" };
+      return { userId };
     })
     .onUploadComplete(({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
