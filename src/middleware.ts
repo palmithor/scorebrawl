e9919b9b65be09a1ff134c1f714cd0f22104ea40
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2023-09-15 07:35:51 +0000

    Allow setting Clerk debug mode

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  debug: process.env.CLERK_DEBUG === "true",
+});
 
 // Stop Middleware running on static files
 export const config = {
