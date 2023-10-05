57cdf4a99a2efbfd9707b02ab67d8d36e60196fb
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2023-10-05 13:16:02 +0000

    Users table and handle cler user webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   debug: process.env.CLERK_DEBUG === "true",
+  publicRoutes: ["/api/webhooks(.*)"],
 });
 
 // Stop Middleware running on static files
