e318d124b4a1a9bc5f9355a0af450b13a2387462
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2023-10-05 13:47:21 +0000

    Ignore webhook routes in auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   debug: process.env.CLERK_DEBUG === "true",
-  publicRoutes: ["/api/webhooks(.*)"],
+  ignoredRoutes: ["/api/webhooks/(.*)"],
 });
 
 // Stop Middleware running on static files
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
