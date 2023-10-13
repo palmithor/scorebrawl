9548bb7fabdf5e9b1a7f1c20ed7a016353a0a3ed
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2023-10-13 11:24:38 +0000

    Remove unused migration

--- migrations/0005_adorable_tomas.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `league_team_player` (
-	`id` text(32) PRIMARY KEY NOT NULL,
-	`league_player_id` text(32) NOT NULL,
-	`team_id` text(32) NOT NULL,
-	`created_at` integer NOT NULL,
-	`updated_at` integer NOT NULL
-);
-
-CREATE UNIQUE INDEX `league_team_player_uq_idx` ON `league_team_player` (`team_id`,`league_player_id`);
