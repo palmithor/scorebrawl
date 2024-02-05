c3ff31b84b328ade4e84f1d6070abe4b1ec4dc88
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-02-05 20:40:25 +0000

    LeagueOverview full page spinner

---
+++ apps/scorebrawl/src/app/(leagues)/loading.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from "@scorebrawl/ui/spinner";
+
+export default function () {
+  return (
+    <div className="grid h-screen place-items-center">
+      <Spinner size="40" />
+    </div>
+  );
+}
