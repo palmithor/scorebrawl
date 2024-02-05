08f4014ed79b0f541be18e2d95cb4c47f5913ffc
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-02-05 13:46:31 +0000

    Move leagues to root path

--- apps/scorebrawl/src/middleware.ts
+++ apps/scorebrawl/src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   debug: process.env.DEBUG === "true",
-  ignoredRoutes: ["/api/webhooks/(.*)", "/"],
+  ignoredRoutes: ["/api/webhooks/(.*)"],
   publicRoutes: ["/api/uploadthing"],
 });
 
