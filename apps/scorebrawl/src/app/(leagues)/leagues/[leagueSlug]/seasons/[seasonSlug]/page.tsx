762ffb97211bb6280da3b9aad8458aac7e5245dc
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-07-12 13:51:55 +0000

    Season Overview: Info Card

--- apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/page.tsx
+++ apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/page.tsx
@@ -1,11 +1,11 @@
-import { StatsCards } from "@/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/components/StatsCards";
+import { DashboardCards } from "@/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/components/DashboardCards";
 
 type PageParams = { params: { leagueSlug: string; seasonSlug: string } };
 
 export default ({ params: { leagueSlug, seasonSlug } }: PageParams) => {
   return (
     <div className="grid gap-6">
-      <StatsCards leagueSlug={leagueSlug} seasonSlug={seasonSlug} />
+      <DashboardCards leagueSlug={leagueSlug} seasonSlug={seasonSlug} />
     </div>
   );
 };
