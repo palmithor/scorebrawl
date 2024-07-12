5044e79ed5c2390ccf0782affabcf458acea1f96
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-07-12 13:51:55 +0000

    Season Overview: Info Card

--- apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/components/DashboardCards.tsx
+++ apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/components/DashboardCards.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { InfoCard } from "./InfoCard";
 import { LatestMatchCard } from "./LatestMatchCard";
 
 type Props = { leagueSlug: string; seasonSlug: string };
 
-export const StatsCards = ({ leagueSlug, seasonSlug }: Props) => (
+export const DashboardCards = ({ leagueSlug, seasonSlug }: Props) => (
   <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+    <InfoCard leagueSlug={leagueSlug} seasonSlug={seasonSlug} />
     <LatestMatchCard leagueSlug={leagueSlug} seasonSlug={seasonSlug} />
   </div>
 );
