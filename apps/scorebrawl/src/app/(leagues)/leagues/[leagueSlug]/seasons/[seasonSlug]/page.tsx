2fdc46ff7ca79600730f21f3dcc3df647a4440af
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-07-15 12:20:29 +0000

    Season Overview: Player Standing

--- apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/page.tsx
+++ apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/page.tsx
@@ -1,11 +1,13 @@
 import { DashboardCards } from "@/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/components/DashboardCards";
+import { SeasonPlayerStanding } from "./components/SeasonPlayerStanding";
 
 type PageParams = { params: { leagueSlug: string; seasonSlug: string } };
 
-export default ({ params: { leagueSlug, seasonSlug } }: PageParams) => {
+export default async ({ params: { leagueSlug, seasonSlug } }: PageParams) => {
   return (
     <div className="grid gap-6">
       <DashboardCards leagueSlug={leagueSlug} seasonSlug={seasonSlug} />
+      <SeasonPlayerStanding leagueSlug={leagueSlug} seasonSlug={seasonSlug} />
     </div>
   );
 };
