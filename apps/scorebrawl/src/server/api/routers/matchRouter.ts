3f0225925313528cfb5f2d9e0bbdce61b7ca7f4e
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-07-15 15:33:08 +0000

    Season Overview: Team Standing

--- apps/scorebrawl/src/server/api/routers/matchRouter.ts
+++ apps/scorebrawl/src/server/api/routers/matchRouter.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
-import { createTRPCRouter, leagueProcedure } from "@/server/api/trpc";
+import { createTRPCRouter, seasonProcedure } from "@/server/api/trpc";
 import { MatchRepository } from "@scorebrawl/db";
 
 export const matchRouter = createTRPCRouter({
-  remove: leagueProcedure
+  remove: seasonProcedure
     .input(
       z.object({
         leagueSlug: z.string(),
@@ -13,7 +13,7 @@ export const matchRouter = createTRPCRouter({
       }),
     )
     .mutation(({ input }) => MatchRepository.remove(input)),
-  getLatest: leagueProcedure
+  getLatest: seasonProcedure
     .input(z.object({ leagueSlug: z.string(), seasonSlug: z.string() }))
     .query(async ({ ctx, input: { seasonSlug } }) =>
       MatchRepository.findLatest({ leagueId: ctx.league.id, seasonSlug }),
--- packages/db/src/repositories/season-team-repository.ts
+++ packages/db/src/repositories/season-team-repository.ts
@@ -1,165 +1,137 @@
-import { endOfDay, startOfDay } from "date-fns";
-import { and, desc, eq, gte, inArray, lte } from "drizzle-orm";
+import { and, desc, eq, sql } from "drizzle-orm";
 import { db } from "../db";
 import {
   leaguePlayers,
   leagueTeamPlayers,
   leagueTeams,
   leagues,
+  matches,
   seasonTeams,
   seasons,
   teamMatches,
   users,
 } from "../schema";
-import { SeasonRepository } from "./season-repository";
+
+const matchesSubqueryBuilder = ({ seasonSlug }: { seasonSlug: string }) =>
+  db
+    .select({
+      seasonTeamId: teamMatches.seasonTeamId,
+      matchId: teamMatches.matchId,
+      result: teamMatches.result,
+      createdAt: teamMatches.createdAt,
+      rowNumber:
+        sql`ROW_NUMBER() OVER (PARTITION BY ${teamMatches.seasonTeamId} ORDER BY ${teamMatches.createdAt} DESC)`.as(
+          "rowNumber",
+        ),
+    })
+    .from(teamMatches)
+    .innerJoin(matches, eq(matches.id, teamMatches.matchId))
+    .innerJoin(seasonTeams, eq(seasonTeams.id, teamMatches.seasonTeamId))
+    .innerJoin(seasons, and(eq(seasons.slug, seasonSlug), eq(seasonTeams.seasonId, seasons.id)))
+    .as("recent_matches");
+
+const getStanding = async ({ seasonSlug }: { seasonSlug: string }) => {
+  const matchesSubquery = matchesSubqueryBuilder({ seasonSlug });
+
+  // New subquery to calculate point difference for the current day
+  const pointDiffSubquery = db
+    .select({
+      seasonTeamId: teamMatches.seasonTeamId,
+      pointDiff: sql<number>`MAX(${teamMatches.scoreAfter}) - MIN(${teamMatches.scoreBefore})`.as(
+        "pointDiff",
+      ),
+    })
+    .from(teamMatches)
+    .innerJoin(matches, eq(matches.id, teamMatches.matchId))
+    .innerJoin(seasons, eq(seasons.id, matches.seasonId))
+    .where(and(eq(seasons.slug, seasonSlug), sql`DATE(${matches.createdAt}) = CURRENT_DATE`))
+    .groupBy(teamMatches.seasonTeamId)
+    .as("point_diff");
+
+  const teamStats = await db
+    .select({
+      seasonTeamId: matchesSubquery.seasonTeamId,
+      totalGames: sql<number>`COUNT(*)`.as("totalGames"),
+      wins: sql<number>`SUM(CASE WHEN ${matchesSubquery.result} = 'W' THEN 1 ELSE 0 END)`.as(
+        "wins",
+      ),
+      losses: sql<number>`SUM(CASE WHEN ${matchesSubquery.result} = 'L' THEN 1 ELSE 0 END)`.as(
+        "losses",
+      ),
+      draws: sql<number>`SUM(CASE WHEN ${matchesSubquery.result} = 'D' THEN 1 ELSE 0 END)`.as(
+        "draws",
+      ),
+      recentResults:
+        sql`STRING_AGG(${matchesSubquery.result}, ',' ORDER BY ${matchesSubquery.createdAt} DESC)`.as(
+          "recentResults",
+        ),
+    })
+    .from(matchesSubquery)
+    .groupBy(matchesSubquery.seasonTeamId);
+
+  const teams = await db
+    .select({
+      name: leagueTeams.name,
+      seasonTeamId: seasonTeams.id,
+      score: seasonTeams.score,
+      pointDiff: pointDiffSubquery.pointDiff,
+    })
+    .from(seasonTeams)
+    .innerJoin(seasons, and(eq(seasons.id, seasonTeams.seasonId), eq(seasons.slug, seasonSlug)))
+    .innerJoin(leagueTeams, eq(leagueTeams.id, seasonTeams.teamId))
+    .leftJoin(pointDiffSubquery, eq(pointDiffSubquery.seasonTeamId, seasonTeams.id))
+    .orderBy(desc(seasonTeams.score));
+
+  return teams.map((p) => {
+    const stats = teamStats.find((ps) => ps.seasonTeamId === p.seasonTeamId);
+    const form = (stats?.recentResults as string)?.split(",")?.slice(0, 5) ?? [];
+
+    return {
+      seasonTeamId: p.seasonTeamId,
+      name: p.name,
+      score: p.score,
+      matchCount: stats?.totalGames ?? 0,
+      winCount: stats?.wins ?? 0,
+      lossCount: stats?.losses ?? 0,
+      drawCount: stats?.draws ?? 0,
+      form: (form as ("W" | "D" | "L")[]).reverse(),
+      pointDiff: p.pointDiff ?? 0, // Add this line to include the point difference
+    };
+  });
+};
 
 const getTopTeam = async ({ seasonSlug }: { seasonSlug: string }) => {
-  const result = await db
+  const topTeamSubquery = db
+    .select({
+      seasonTeamId: seasonTeams.id,
+      maxScore: seasonTeams.score,
+    })
+    .from(seasonTeams)
+    .innerJoin(seasons, and(eq(seasons.id, seasonTeams.seasonId), eq(seasons.slug, seasonSlug)))
+    .orderBy(desc(seasonTeams.score))
+    .limit(1)
+    .as("top_team");
+
+  return db
     .select({
       seasonTeamId: seasonTeams.id,
       id: users.id,
       teamName: leagueTeams.name,
       name: users.name,
       imageUrl: users.imageUrl,
+      score: seasonTeams.score,
     })
     .from(seasonTeams)
+    .innerJoin(topTeamSubquery, eq(topTeamSubquery.seasonTeamId, seasonTeams.id))
     .innerJoin(seasons, and(eq(seasons.id, seasonTeams.seasonId), eq(seasons.slug, seasonSlug)))
     .innerJoin(leagues, eq(seasons.leagueId, leagues.id))
     .innerJoin(leagueTeams, eq(leagueTeams.id, seasonTeams.teamId))
     .innerJoin(leagueTeamPlayers, eq(leagueTeamPlayers.teamId, leagueTeams.id))
     .innerJoin(leaguePlayers, eq(leaguePlayers.id, leagueTeamPlayers.leaguePlayerId))
-    .innerJoin(users, eq(users.id, leaguePlayers.userId))
-    .orderBy(desc(seasonTeams.score));
-
-  const topTeamId = result[0]?.seasonTeamId;
-  return topTeamId ? result.filter((r) => r.seasonTeamId === topTeamId) : [];
-};
-
-const getTeamsPointDiff = async ({
-  seasonTeamIds,
-  from = startOfDay(new Date()),
-  to = endOfDay(new Date()),
-}: { seasonTeamIds: string[]; from?: Date; to?: Date }) => {
-  const result = await db.query.teamMatches.findMany({
-    where: (matchPlayer, { and }) =>
-      and(
-        inArray(teamMatches.seasonTeamId, seasonTeamIds),
-        gte(matchPlayer.createdAt, from),
-        lte(matchPlayer.createdAt, to),
-      ),
-    orderBy: (matchPlayer, { asc }) => [asc(matchPlayer.createdAt)],
-  });
-  if (result.length === 0) {
-    return [];
-  }
-  type MatchTeamType = (typeof result)[0];
-  type SeasonTeamMatches = { seasonTeamId: string; matches: MatchTeamType[] };
-
-  const seasonTeamMatches = result.reduce((acc: SeasonTeamMatches[], curr: MatchTeamType) => {
-    const { seasonTeamId } = curr;
-    const index = acc.findIndex((item: SeasonTeamMatches) => item.seasonTeamId === seasonTeamId);
-    index !== -1 ? acc[index]?.matches.push(curr) : acc.push({ seasonTeamId, matches: [curr] });
-    return acc;
-  }, []);
-
-  return seasonTeamMatches.map((spm) => ({
-    seasonTeamId: spm.seasonTeamId,
-    pointsDiff:
-      (spm.matches[spm.matches.length - 1]?.scoreAfter ?? 0) - (spm.matches[0]?.scoreBefore ?? 0),
-  }));
-};
-
-const getTeamsLatestMatches = async ({
-  seasonTeamIds,
-  limit = 5,
-}: {
-  seasonTeamIds: string[];
-  limit?: number;
-}) => {
-  return db.query.seasonTeams.findMany({
-    columns: { id: true },
-    where: inArray(seasonTeams.id, seasonTeamIds),
-    with: {
-      season: { columns: { id: true } },
-      matches: {
-        orderBy: (match, { desc }) => [desc(match.createdAt)],
-        limit,
-      },
-    },
-  });
-};
-
-const getTeams = async ({
-  leagueId,
-  seasonId,
-  userId,
-}: {
-  leagueId: string;
-  seasonId: string;
-  userId: string;
-}) => {
-  const season = await SeasonRepository.getById({ seasonId, userId, leagueId });
-  const teams = await db.query.seasonTeams.findMany({
-    extras: (_seasonTeam, { sql }) => ({
-      matchCount:
-        sql<number>`(SELECT COUNT(*) FROM season_team_match stm WHERE stm.season_team_id = "seasonTeams"."id")`.as(
-          "matchCount",
-        ),
-      winCount:
-        sql<number>`(SELECT COUNT(*) FROM season_team_match stm WHERE stm.season_team_id = "seasonTeams"."id" and result = 'W')`.as(
-          "winCount",
-        ),
-      lossCount:
-        sql<number>`(SELECT COUNT(*) FROM season_team_match stm WHERE stm.season_team_id = "seasonTeams"."id" and result = 'L')`.as(
-          "lossCount",
-        ),
-      drawCount:
-        sql<number>`(SELECT COUNT(*) FROM season_team_match stm WHERE stm.season_team_id = "seasonTeams"."id" and result = 'D')`.as(
-          "drawCount",
-        ),
-    }),
-    where: eq(seasonTeams.seasonId, season.id),
-    columns: { id: true, score: true, createdAt: true, updatedAt: true },
-    orderBy: desc(seasonTeams.score),
-    with: {
-      leagueTeam: {
-        columns: { id: true, name: true },
-        with: {
-          players: {
-            columns: { id: true },
-            with: {
-              leaguePlayer: {
-                columns: { id: true },
-                with: { user: { columns: { name: true, imageUrl: true } } },
-              },
-            },
-          },
-        },
-      },
-    },
-  });
-  return teams.map((team) => ({
-    id: team.id,
-    leagueTeamId: team.leagueTeam.id,
-    name: team.leagueTeam.name,
-    score: team.score,
-    players: team.leagueTeam.players.map((p) => ({
-      id: p.leaguePlayer.id,
-      name: p.leaguePlayer.user.name,
-      imageUrl: p.leaguePlayer.user.imageUrl,
-    })),
-    matchCount: team.matchCount,
-    winCount: team.winCount,
-    lossCount: team.lossCount,
-    drawCount: team.drawCount,
-    createdAt: team.createdAt,
-    updatedAt: team.updatedAt,
-  }));
+    .innerJoin(users, eq(users.id, leaguePlayers.userId));
 };
 
 export const SeasonTeamRepository = {
+  getStanding,
   getTopTeam,
-  getTeams,
-  getTeamsPointDiff,
-  getTeamsLatestMatches,
 };
