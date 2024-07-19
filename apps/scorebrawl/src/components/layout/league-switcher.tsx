02d3802176ecea6cc4cce7a06233362bcf45b143
jonthor <jonthor@tempo.io>
2024-07-19 14:48:48 +0000

    Sidebar Nav Layout
    Co-authored-by: Pálmi Þór Valgeirsson <palmiv@tempo.io>

---
+++ apps/scorebrawl/src/components/layout/league-switcher.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { Avatar, AvatarFallback, AvatarImage } from "@scorebrawl/ui/avatar";
+import { cn } from "@scorebrawl/ui/lib";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@scorebrawl/ui/select";
+import { Separator } from "@scorebrawl/ui/separator";
+import { useRouter } from "next/navigation";
+
+interface LeagueSwitcherProps {
+  isCollapsed: boolean;
+  leagues: { id: string; slug: string; name: string; logoUrl: string | null }[];
+  selectedLeague?: { id: string; slug: string; name: string; logoUrl: string | null };
+  onLeagueSelect: (leagueId: string) => void;
+}
+
+export function LeagueSwitcher({
+  isCollapsed,
+  leagues,
+  selectedLeague,
+  onLeagueSelect,
+}: LeagueSwitcherProps) {
+  const router = useRouter();
+  return (
+    <Select
+      defaultValue={selectedLeague?.name}
+      onValueChange={(value) => {
+        if (value === "create") {
+          router.push("/leagues/create");
+        } else {
+          document.cookie = `last-visited-league=${value}`;
+          onLeagueSelect(value);
+        }
+      }}
+    >
+      <SelectTrigger
+        className={cn(
+          "flex items-center gap-2 [&>span]:line-clamp-1 [&>span]:flex [&>span]:w-full [&>span]:items-center [&>span]:gap-1 [&>span]:truncate [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0",
+          isCollapsed &&
+            "flex h-9 w-9 shrink-0 items-center justify-center p-0 [&>span]:w-auto [&>svg]:hidden",
+        )}
+        aria-label="Select League"
+      >
+        <SelectValue placeholder="Select a league">
+          <Avatar className="h-6 w-6">
+            <AvatarImage src={selectedLeague?.logoUrl ?? ""} />
+            <AvatarFallback>{`${selectedLeague?.name.charAt(0).toUpperCase()}`}</AvatarFallback>
+          </Avatar>
+          <span className={cn("ml-1", isCollapsed && "hidden")}>{selectedLeague?.name}</span>
+        </SelectValue>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Leagues</SelectLabel>
+          {leagues.map((league) => (
+            <SelectItem key={league.slug} value={league.slug}>
+              <div className="flex items-center gap-2 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src={league.logoUrl ?? ""} />
+                  <AvatarFallback>{`${league.name.charAt(0).toUpperCase()}`}</AvatarFallback>
+                </Avatar>
+                {league.name}
+              </div>
+            </SelectItem>
+          ))}
+        </SelectGroup>
+        <Separator className="my-2" />
+        <SelectGroup>
+          <SelectItem className="mt-2 " value="create">
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarFallback>
+                  <PlusIcon className="h-5 w-5" />
+                </AvatarFallback>
+              </Avatar>
+              Create League
+            </div>
+          </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
--- apps/scorebrawl/src/components/layout/sub-nav.tsx
+++ apps/scorebrawl/src/components/layout/sub-nav.tsx
@@ -1,38 +1,19 @@
 "use client";
 
 import { cn } from "@scorebrawl/ui/lib";
-import { ScrollArea, ScrollBar } from "@scorebrawl/ui/scroll-area";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import type { ReactNode } from "react";
 
-type SubNavProps = React.HTMLAttributes<HTMLDivElement> & {
-  links: { name: string; href: string }[];
+type SubNavProps = {
+  seasonName?: string;
+  children: ReactNode;
 };
 
-export function SubNav({ links, children, className, ...props }: SubNavProps) {
-  const pathname = usePathname();
-
+export function SubNav({ seasonName, children }: SubNavProps) {
   return (
-    <div className="flex items-center sm:gap-4 py-2 sticky top-12 bg-background/80 backdrop-blur-md z-20">
-      <ScrollArea className="grow lg:max-w-none">
-        <div className={cn("flex items-center", className)} {...props}>
-          {links.map((link, index) => (
-            <Link
-              href={link.href}
-              key={link.href}
-              className={cn(
-                "flex h-7 items-center justify-center rounded-full px-2 sm:px-4 text-center text-sm transition-colors hover:text-primary",
-                pathname?.startsWith(link.href) || (index === 0 && pathname === "/")
-                  ? "bg-muted font-medium text-primary"
-                  : "text-muted-foreground",
-              )}
-            >
-              {link.name}
-            </Link>
-          ))}
-        </div>
-        <ScrollBar orientation="horizontal" className="invisible" />
-      </ScrollArea>
+    <div className="flex items-center sm:gap-4 py-2 sticky top-0 bg-background/80 backdrop-blur-md z-20 justify-between">
+      <h1 className={cn("text-xl font-bold tracking-tight")}>
+        {seasonName ?? "No season for league"}
+      </h1>
       <div className="flex">{children}</div>
     </div>
   );
