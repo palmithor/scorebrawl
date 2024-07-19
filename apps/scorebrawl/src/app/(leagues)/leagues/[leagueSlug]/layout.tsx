7f5f09535d45929abd264bd3f3ead5d00bfa1c3b
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-07-19 13:45:55 +0000

    Season Overview: Redirect league home / overview to ongoing season overview

--- apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/layout.tsx
+++ apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/layout.tsx
@@ -5,19 +5,20 @@ import {
   getLeagueBySlugWithUserRoleOrRedirect,
   getPlayers,
 } from "@/actions/league";
-import { findOngoing, getPlayers as getSeasonPlayers } from "@/actions/season";
+import { getPlayers as getSeasonPlayers } from "@/actions/season";
 import { LeagueDetailsSubNav } from "@/components/league/league-details-sub-nav";
+import { api } from "@/trpc/server";
 import { auth } from "@clerk/nextjs/server";
 import type { Metadata, ResolvingMetadata } from "next";
 import type { ReactNode } from "react";
 
 export async function generateMetadata(
-  { params }: { params: { leagueSlug: string } },
+  { params: { leagueSlug } }: { params: { leagueSlug: string } },
   _parent: ResolvingMetadata,
 ): Promise<Metadata> {
   const league = { name: "" };
   try {
-    const leagueBySlug = await findBySlug(params.leagueSlug);
+    const leagueBySlug = await findBySlug(leagueSlug);
     league.name = leagueBySlug?.name ?? "Unknown";
   } catch (_e) {
     // ignore
@@ -29,15 +30,15 @@ export async function generateMetadata(
 }
 
 export default async function ({
-  params,
+  params: { leagueSlug },
   children,
 }: {
   params: { leagueSlug: string };
   children: ReactNode;
 }) {
-  const league = await getLeagueBySlugWithUserRoleOrRedirect(params.leagueSlug);
+  const league = await getLeagueBySlugWithUserRoleOrRedirect(leagueSlug);
   const leaguePlayers = await getPlayers(league.id);
-  const ongoingSeason = await findOngoing(league.id);
+  const ongoingSeason = await api.season.findOngoing({ leagueSlug });
   const ongoingSeasonPlayers = ongoingSeason
     ? await getSeasonPlayers(ongoingSeason.id, league.id)
     : [];
@@ -53,8 +54,8 @@ export default async function ({
         shouldShowJoin={!!(code && !leaguePlayers.some((u) => u?.userId === userId))}
         hasEditorAccess={hasEditorAccess}
         inviteCode={code}
-        shouldEnableAddMatch={hasTwoPlayersOrMore && !!ongoingSeason}
-        shouldShowAddMatch={leaguePlayers?.some((p) => p.userId === userId)}
+        ongoingSeason={ongoingSeason}
+        hasTwoPlayersOrMore={hasTwoPlayersOrMore}
       />
       {children}
     </>
