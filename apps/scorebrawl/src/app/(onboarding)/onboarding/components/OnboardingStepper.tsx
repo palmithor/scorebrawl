a30a13789f3f1b028cbab09aac39fb3c957a7ce3
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-08-01 14:15:06 +0000

    Onboarding layout

---
+++ apps/scorebrawl/src/app/(onboarding)/onboarding/components/OnboardingStepper.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { GetStartedStep } from "@/app/(onboarding)/onboarding/components/GetStartedStep";
+import { ProfileStep } from "@/app/(onboarding)/onboarding/components/ProfileStep";
+import { WelcomeStep } from "@/app/(onboarding)/onboarding/components/WelcomeStep";
+import { Card } from "@scorebrawl/ui/card";
+import { Step, type StepItem, Stepper } from "@scorebrawl/ui/stepper";
+import type { ReactNode } from "react";
+import { Footer } from "./Footer";
+
+const steps = [
+  { label: "Welcome", component: <WelcomeStep /> },
+  { label: "Profile", component: <ProfileStep /> },
+  { label: "Get Started", component: <GetStartedStep /> },
+] satisfies (StepItem & { component: ReactNode })[];
+export const OnboardingStepper = () => {
+  return (
+    <Stepper initialStep={0} steps={steps}>
+      {steps.map(({ label, component }, _index) => {
+        return (
+          <Step key={label} label={label}>
+            <Card>{component}</Card>
+          </Step>
+        );
+      })}
+      <Footer />
+    </Stepper>
+  );
+};
---
+++ apps/scorebrawl/src/app/(onboarding)/onboarding/components/ProfileStep.tsx
@@ -0,0 +1,18 @@
+import { CardContent, CardDescription, CardHeader, CardTitle } from "@scorebrawl/ui/card";
+
+export const ProfileStep = () => {
+  return (
+    <>
+      <CardHeader>
+        <CardTitle>Who are you?</CardTitle>
+        <CardDescription>Customize your profile</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
+          ullamcorper mattis, pulvinar dapibus leo.
+        </p>
+      </CardContent>
+    </>
+  );
+};
