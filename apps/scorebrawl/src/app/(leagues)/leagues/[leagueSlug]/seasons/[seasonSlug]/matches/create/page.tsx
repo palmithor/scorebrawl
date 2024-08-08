6e28e6155348c92953c9f1482bdb91cfb893dc73
jonthor <jonthor@tempo.io>
2024-08-08 22:44:28 +0000

    Breadcrumbs header

--- apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/matches/create/page.tsx
+++ apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/matches/create/page.tsx
@@ -1,11 +1,24 @@
+import { findSeasonBySlug } from "@/actions/season";
 import { SeasonPlayerStanding } from "@/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/components/SeasonPlayerStanding";
+import { BreadcrumbsHeader } from "@/components/layout/breadcrumbs-header";
 import { MatchForm } from "./components/MatchForm";
 
-export default async function () {
+type PageParams = { params: { leagueSlug: string; seasonSlug: string } };
+
+export default async ({ params: { leagueSlug, seasonSlug } }: PageParams) => {
+  const season = await findSeasonBySlug(leagueSlug, seasonSlug);
   return (
     <div className="grid gap-3">
+      <BreadcrumbsHeader
+        breadcrumbs={[
+          { name: "Seasons", href: `/leagues/${leagueSlug}/seasons` },
+          { name: season.name, href: `/leagues/${leagueSlug}/seasons/${seasonSlug}` },
+          { name: "Matches", href: `/leagues/${leagueSlug}/seasons/${seasonSlug}/matches` },
+          { name: "Create" },
+        ]}
+      />
       <MatchForm />
       <SeasonPlayerStanding />
     </div>
   );
-}
+};
---
+++ apps/scorebrawl/src/components/layout/breadcrumbs-header.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@scorebrawl/ui/breadcrumbs";
+import Link from "next/link";
+import type { ReactNode } from "react";
+
+export const BreadcrumbsHeader = ({
+  breadcrumbs,
+  children,
+}: {
+  breadcrumbs: {
+    name: string;
+    href?: string;
+  }[];
+  children?: ReactNode;
+}) => (
+  <header className="sticky top-0 z-30 grid grid-cols-2 grid-rows-1 h-14 items-center gap-4 border-l-amber-50 bg-background py-2 px-4 sm:static  sm:border-0 sm:bg-transparent sm:px-6 truncate">
+    <Breadcrumb className="flex trunctate">
+      <BreadcrumbList className="flex-nowrap">
+        {breadcrumbs.map((crumb, index) => (
+          <BreadcrumbItem key={`item-${crumb.name}-${crumb.href}`}>
+            {crumb.href ? (
+              <>
+                <BreadcrumbLink asChild>
+                  <Link href={crumb.href} prefetch={false}>
+                    {crumb.name}
+                  </Link>
+                </BreadcrumbLink>
+                {index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+              </>
+            ) : (
+              <BreadcrumbPage className="truncate">{crumb.name}</BreadcrumbPage>
+            )}
+          </BreadcrumbItem>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+    <div className="ml-auto flex items-center gap-2">{children}</div>
+  </header>
+);
