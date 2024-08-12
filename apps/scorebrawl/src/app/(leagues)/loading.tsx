ac3d309ed9fab974277cc50a1efe1198c1d29038
jonthor <jonthor@tempo.io>
2024-08-12 15:06:46 +0000

    Fix layout so that the page is always screen height and content is scrolled if it extends the height. Also, change header so its always sticky when scrolling Fix loading, so it does not trigger a blinking scrollbar

--- apps/scorebrawl/src/app/(leagues)/loading.tsx
+++ apps/scorebrawl/src/app/(leagues)/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@scorebrawl/ui/spinner";
 
 export default function () {
   return (
-    <div className="grid h-screen place-items-center">
+    <div className="flex absolute h-screen w-full items-center justify-center">
       <Spinner size="40" />
     </div>
   );
