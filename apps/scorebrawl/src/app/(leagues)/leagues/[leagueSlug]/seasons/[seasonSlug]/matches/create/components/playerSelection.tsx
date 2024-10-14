721d55f7c8fa2a4ff99e61e7c16b259740050af1
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-10-14 12:12:45 +0000

    Revert broken change in player selection

--- apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/matches/create/components/playerSelection.tsx
+++ apps/scorebrawl/src/app/(leagues)/leagues/[leagueSlug]/seasons/[seasonSlug]/matches/create/components/playerSelection.tsx
@@ -1,4 +1,6 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { cn } from "@/lib/utils";
+import { getInitialsFromString } from "@scorebrawl/utils/string";
 import type { PlayerWithSelection } from "./MatchForm";
 
 export const PlayerSelection = ({
@@ -17,7 +19,7 @@ export const PlayerSelection = ({
   return (
     <div className="flex flex-col gap-0.5">
       {players.map((p) => (
-        <input
+        <button
           key={p.user.userId}
           onClick={() => handlePlayerSelection(p)}
           onKeyDown={(e) => e.key === "Enter" && handlePlayerSelection(p)}
@@ -27,7 +29,16 @@ export const PlayerSelection = ({
             "flex gap-2 items-center p-1 rounded-md",
             p.team ? (p.team === team ? "bg-primary/20" : "line-through") : "",
           )}
-        />
+        >
+          <Avatar className="h-6 w-6">
+            <AvatarImage src={p.user.imageUrl} />
+            <AvatarFallback>{getInitialsFromString(p.user.name)}</AvatarFallback>
+          </Avatar>
+          <div className="grid auto-rows-min text-start">
+            <p className="text-xs font-medium truncate">{p.user.name}</p>
+            <p className="text-xs text-muted-foreground">{p.score}</p>
+          </div>
+        </button>
       ))}
     </div>
   );
