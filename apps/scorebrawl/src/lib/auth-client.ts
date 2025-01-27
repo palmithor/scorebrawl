565ffbcff2727487e214bd99d71f8bf1640bca30
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2025-01-27 15:04:09 +0000

    Bun text lockfile

--- apps/scorebrawl/src/lib/auth-client.ts
+++ apps/scorebrawl/src/lib/auth-client.ts
@@ -1,5 +1,17 @@
-import { env } from "@/env.mjs";
 import { createAuthClient } from "better-auth/react";
+
+export const getURL = () => {
+  const url =
+    (process.env.NEXT_PUBLIC_EXPORT
+      ? process.env.NEXT_PUBLIC_BASE_URL
+      : process.env.NEXT_PUBLIC_SITE_URL || // Set this to your site URL in production env only. (Production)
+        process.env.NEXT_PUBLIC_VERCEL_URL) || // Automatically set by Vercel. (Preview)
+    "http://localhost:5050"; // Default to localhost. (Development)
+
+  // Make sure to include `https://` when not localhost.
+  return url.includes("http") ? url : `https://${url}`;
+};
+
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  baseURL: getURL(),
 });
