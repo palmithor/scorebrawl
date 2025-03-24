d6f1a7a8ec7c5e39bb3737f747fb6c166f9dccb4
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2025-03-24 16:03:39 +0000

    Redirect to auth in invite - fix

---
+++ apps/scorebrawl/src/app/(auth)/auth/components/LoginWithGoogle.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useQueryState } from "nuqs";
+
+export const LoginWithGoogle = () => {
+  const [rt] = useQueryState("rt", { defaultValue: "" });
+  console.log(rt);
+  const signInWithGoogle = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: rt ?? undefined,
+    });
+  };
+  return (
+    <Button onClick={signInWithGoogle} variant="outline" className="w-full text-lg">
+      Sign in with Google
+    </Button>
+  );
+};
