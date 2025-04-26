d8f0cd5404a1f0d3a408b8b8228b18d1a3a170e7
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2025-04-26 11:45:24 +0000

    Upstash drizzle cache

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,9 +1,11 @@
 import { neon } from "@neondatabase/serverless";
+import { upstashCache } from "drizzle-orm/cache/upstash";
 import { drizzle } from "drizzle-orm/neon-http";
 import { migrate } from "drizzle-orm/neon-http/migrator";
 import { type PostgresJsDatabase, drizzle as localDrizzle } from "drizzle-orm/postgres-js";
 import { migrate as localMigrator } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
+
 import * as schema from "./schema";
 
 const devDb = (): PostgresJsDatabase<typeof schema> => {
@@ -21,6 +23,14 @@ const databaseUrl = process.env.DATABASE_URL ?? "";
 export const db = process.env.VERCEL
   ? drizzle(neon(databaseUrl), {
       schema,
+      cache:
+        process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN
+          ? upstashCache({
+              url: process.env.UPSTASH_REDIS_REST_URL,
+              token: process.env.UPSTASH_REDIS_REST_TOKEN,
+              global: true,
+            })
+          : undefined,
     })
   : devDb();
 
