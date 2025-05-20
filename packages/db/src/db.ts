829c54756e36e5b40a3f2f0a43e2477c1e259f8d
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2025-05-20 08:56:34 +0000

    Revert "Supabase"
    This reverts commit 4ee6874180e1336031c3da2f630407bdc5b04323.

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,24 +1,46 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-
+import { neon } from "@neondatabase/serverless";
 import { upstashCache } from "drizzle-orm/cache/upstash";
+import { drizzle } from "drizzle-orm/neon-http";
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { type PostgresJsDatabase, drizzle as localDrizzle } from "drizzle-orm/postgres-js";
+import { migrate as localMigrator } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+
 import * as schema from "./schema";
 
+const devDb = (): PostgresJsDatabase<typeof schema> => {
+  if (globalThis.dbCache) {
+    return globalThis.dbCache;
+  }
+  globalThis.dbCache = localDrizzle(postgres(databaseUrl), {
+    schema,
+    logger: true,
+  });
+  return globalThis.dbCache;
+};
+
 const databaseUrl = process.env.DATABASE_URL ?? "";
-export const db = drizzle(databaseUrl, {
-  schema,
-  cache:
-    process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN
-      ? upstashCache({
-          url: process.env.UPSTASH_REDIS_REST_URL,
-          token: process.env.UPSTASH_REDIS_REST_TOKEN,
-          global: true,
-        })
-      : undefined,
-});
+export const db = process.env.VERCEL
+  ? drizzle(neon(databaseUrl), {
+      schema,
+      cache:
+        process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN
+          ? upstashCache({
+              url: process.env.UPSTASH_REDIS_REST_URL,
+              token: process.env.UPSTASH_REDIS_REST_TOKEN,
+              global: true,
+            })
+          : undefined,
+    })
+  : devDb();
 
 export const migrateDb = async () => {
-  await migrate(drizzle(databaseUrl), {
-    migrationsFolder: "./migrations",
-  });
+  if (process.env.VERCEL) {
+    await migrate(drizzle(neon(databaseUrl)), {
+      migrationsFolder: "./migrations",
+    });
+  } else {
+    const migrateDrizzle = localDrizzle(postgres(databaseUrl, { max: 1 }));
+    await localMigrator(migrateDrizzle, { migrationsFolder: "./migrations" });
+  }
 };
